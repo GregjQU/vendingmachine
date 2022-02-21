d3fbbe7c45a8814b5bd9bfc5abf68df38a6dc0f6
GregjQU <gjeanbaptiste@qu.edu>
2022-02-20 19:43:30 -0500

    starting template.

--- src/sharedreep/vending.java
+++ src/sharedreep/vending.java
@@ -1,10 +1,24 @@
 package sharedreep;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class vending {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+System.out.println("Welcome to the vending machine! Please select an item");
 
+char snacks;
+float snack1, snack2,snack3;
+System.out.println("Options: ");
+snack1 = scanner.nextFloat();
+
+System.out.println("Please insert the required amount");
+
+		
+		
 	}
 
 }
